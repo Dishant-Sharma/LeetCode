c2a90722e7b4f1ff5c7324db9ee9e320f2cfbc70
Dishant Sharma <dishantsharma44.ds@gmail.com>
2022-05-27 17:44:27 +0530

    Time: 44 ms (84.81%), Space: 14 MB (7.23%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
@@ -3,5 +3,10 @@ class Solution:
         return max(s.count(" ") for s in sentences) + 1
         # return max(s.count(" ") for s in sentences)
         
+        x=0
+        for word in sentences:
+            x= max(s.count(" ") +1 )
+        return x
+        
         
     
