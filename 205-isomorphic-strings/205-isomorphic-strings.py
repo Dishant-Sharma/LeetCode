6c46412dde29379fb40e7a7264fb7d048e3c7aa3
Dishant Sharma <dishantsharma44.ds@gmail.com>
2022-09-24 17:42:47 +0530

    Time: 34 ms (98.98%), Space: 14.1 MB (88.57%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -2,10 +2,10 @@ class Solution:
 	def isIsomorphic(self, s: str, t: str) -> bool:
 		if len(set(s)) != len(set(t)):
 			return False
-		hash_map = {}
-		for char in range(len(t)):
-			if t[char] not in hash_map:
-				hash_map[t[char]] = s[char]
-			elif hash_map[t[char]] != s[char]:
+		dict = {}
+		for i in range(len(t)):
+			if t[i] not in dict:
+				dict[t[i]] = s[i]
+			elif dict[t[i]] != s[i]:
 				return False
 		return True
