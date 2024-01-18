20561d442244217ba8fae908d0a4053f2a046d47
Dishant Sharma <dishantsharma44.ds@gmail.com>
2024-01-18 22:43:42 +0530

    Time: 20 ms (70.70%), Space: 26.8 MB (23.84%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        Dictionary<int, int> memo = new Dictionary<int, int>();
+        return TotalWaysPossible(0, n, memo);
+    }
+
+    private int TotalWaysPossible(int cS, int tS, Dictionary<int, int> memo)
+    {
+        if (cS == tS)
+        {
+            return 1;
+        }
+        if (cS > tS)
+        {
+            return 0;
+        }
+
+        int currentKey = cS;
+
+        if (memo.ContainsKey(currentKey))
+        {
+            return memo[currentKey];
+        }
+
+        int oneJump = TotalWaysPossible(cS + 1, tS, memo);
+        int twoJumps = TotalWaysPossible(cS + 2, tS, memo);
+
+        memo[currentKey] = oneJump + twoJumps;
+        return oneJump + twoJumps;
+    }
+}
+
+
