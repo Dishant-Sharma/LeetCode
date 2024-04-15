6125743ae94d1342985ca14b75a220ea404b6a83
Dishant Sharma <dishantsharma44.ds@gmail.com>
2024-04-16 02:56:55 +0530

    Time: 2 ms (84.40%), Space: 42.1 MB (54.02%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,13 +1,29 @@
 class Solution {
-    public String makeGood(String s) { String t = "";
-        while(s != t) {
-            t = s;
-            for(int i = 0; i < s.length() - 1; i++) {
-                if(s.charAt(i) == s.charAt(i + 1) + 32 || s.charAt(i) + 32 == s.charAt(i + 1)) {
-                    s = s.substring(0, i) + s.substring(i + 2);
+    public String makeGood(String s) {
+        StringBuilder sb = new StringBuilder();
+//        boolean hasChanged = true;
+////        while (hasChanged) {
+//            hasChanged = false;
+
+            for (char c : s.toCharArray()) {
+                if (sb.length() > 0 && Character.toLowerCase(sb.charAt(sb.length() - 1)) == Character.toLowerCase(c) && sb.charAt(sb.length() - 1) != c) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                    sb.append(c);
                 }
-            }
+//            }
         }
-        return s;
+        return sb.toString();
+    
+        // String t = "";
+        // while(s != t) {
+        //     t = s;
+        //     for(int i = 0; i < s.length() - 1; i++) {
+        //         if(s.charAt(i) == s.charAt(i + 1) + 32 || s.charAt(i) + 32 == s.charAt(i + 1)) {
+        //             s = s.substring(0, i) + s.substring(i + 2);
+        //         }
+        //     }
+        // }
+        // return s;
     }
 }
